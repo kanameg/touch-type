4ed8d01ac4c7dbd6b6fffc37ad83447e8548688c
kanameg <kaname.g@gmail.com>
2023-01-14 03:45:57 +0900

    headerとresultの結果を関数化

--- tt.py
+++ tt.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 import argparse
 import csv
 from datetime import datetime, date
@@ -13,20 +13,46 @@ GREEN = '\033[32m'
 END = '\033[0m'
 
 DEFAULT_TYPE_NUM = 20
-DEFAULT_TYPE_KEY = 's'
+
 TYPE_LOG_NAME = "touch_type.csv"
 RESULT_LOG_NAME = "touch_result.csv"
+
+
+# -------------------------------------------------------------------
+# define fuctions
+# -------------------------------------------------------------------
+def print_header(typing_num):
+    """
+    """
+    print("-"*80)
+    print(f"Touch Typing Practice ({typing_num} charactors)")
+    print("-"*80)
+
+
+def print_result(typing_num, match_num, miss_num, typing_time):
+    """
+    """
+    match_rate = match_num / typing_num
+    miss_rate = miss_num / typing_num
+
+    print("-"*80)
+    print("number of correct types: " + GREEN + f"{match_num}" + f" ({match_rate*100}%)" + END)
+    print("number of miss types:    " + RED + f"{miss_num}" + f" ({miss_rate*100}%)" + END)
+    print("time: " + f"{typing_time:.2f} sec" + f"   type speed: {typing_num/typing_time:.2f} char/sec")
+    print("-"*80)
+
+
 
 # -------------------------------------------------------------------
 # parse and get command options
 # -------------------------------------------------------------------
 argp = argparse.ArgumentParser(description='default')
-argp.add_argument('-n', '--num', help='number of practice types')
+argp.add_argument('-n', '--num', help='number of practice typing')
 args = argp.parse_args()
 if args.num:
-    type_num = int(args.num)
+    typing_num = int(args.num)
 else:
-    type_num = DEFAULT_TYPE_NUM
+    typing_num = DEFAULT_TYPE_NUM
 
 # 練習文字の読み込み
 with open('alphabet.json', 'r') as f:
@@ -34,27 +60,20 @@ with open('alphabet.json', 'r') as f:
     test_chars = list(test_chars_dict.keys())
 
 
-def header_print(type_num):
-    """
-    """
-    print("-"*80)
-    print(f"touch typing practice ({type_num} charactors)")
-    print("-"*80)
-
 while True:
-    header_print(type_num)
+    print_header(typing_num)
     print("Press space to strat, press 'Q' to end.")
     command_key = readchar.readkey()
 
     if command_key == ' ':
         match_num = 0
         miss_num = 0
-        today = date.today()
+        now_date = date.today()
         with open(TYPE_LOG_NAME, 'a') as f:
             writer = csv.writer(f)
 
             time_start = time.time() # 時間計測開始
-            for i in range(type_num):
+            for i in range(typing_num):
                 test_char = random.choice(test_chars)
                 answer_key = test_chars_dict[test_char]
                 print(f"{test_char}")
@@ -73,21 +92,15 @@ while True:
                     miss_num = miss_num + 1
                 print('input key is ' +  f"{input_key}" + COLOR + f" ({mark})" + END )
                 now_time = datetime.now().strftime("%H:%M:%S")
-                writer.writerow([today, now_time, i, answer_key, input_key, mark])            
+                writer.writerow([now_date, now_time, i, answer_key, input_key, mark])            
 
-        time_end = time.time() # 計測終了
-        time_type = time_end - time_start
+        time_end = time.time() # 時間計測終了
+        typing_time = time_end - time_start # タイピング時間
 
-        match_rate = match_num / type_num
-        miss_rate = miss_num / type_num
-        print("-"*80)
-        print("number of correct types: " + GREEN + f"{match_num}" + f" ({match_rate*100}%)" + END)
-        print("number of miss types:    " + RED + f"{miss_num}" + f" ({miss_rate*100}%)" + END)
-        print("time: " + f"{time_type:.2f} sec" + f"   type speed: {type_num/time_type:.2f} char/sec")
-        print("-"*80)
+        print_result(typing_num, match_num, miss_num, typing_time)
         with open(RESULT_LOG_NAME, 'a') as f:
             writer = csv.writer(f)
-            writer.writerow([today, now_time, type_num, match_num, miss_num, time_type, type_num/time_type])
+            writer.writerow([now_date, now_time, typing_num, match_num, miss_num, typing_time, typing_num/typing_time])
 
     elif command_key == 'q' or command_key == 'Q':
         break
